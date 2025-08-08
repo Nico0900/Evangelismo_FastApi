48a5ace84eda8736e637ce0cac6c77de1f0d7b86
Nicolas Vargas <161766856+Nico0900@users.noreply.github.com>
2025-08-08 10:45:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI (
+    title = "Evangelismo API"
+    description = "Esta api esta dedicada a..."
+)
+
 
 @app.get("/")
 def read_root():
